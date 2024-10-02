d5e9184e966284fc5866333de3b787701d5bc2ee
EthanGibson18 <egibson4@bucs.fsw.edu>
2024-10-02 17:31:07 -0400

    Corrected conversions and seperated into different categories

--- Java - Unit Conversion/src/Main.java
+++ Java - Unit Conversion/src/Main.java
@@ -2,16 +2,31 @@ public class Main
 {
     public static void main(String[] args)
     {
-        float inch = inch;
-        float foot = inch * 12;
+        float inchToFoot = inch / 12;
+        float inchToCubit = inch / 18;
+        float inchToStep = (inch / 12) / 2.5;
+        float inchToYard = (inch / 12) / 3;
+        float inchToMile = (inch / 12) / 5280;
+
+        float footToInch = foot * 12;
+        float footToCubit =
+
+
         float cubit = inch * 18;
+
+
         float step = (float) (foot * 2.5);
+
+
         float yard = foot * 3;
+
+
         float mile = foot * 5280;
 
-        float inch = foot/12;
+
+        conversion =
 
         // 12 inches = 1 foot, 18 inches = 1 cubit, 2.5 feet = 1 step, 3 feet = 1 yard, 5280 feet = 1 mile
-        System.out.print(inch);
+        System.out.print(conversion);
     }
 }
