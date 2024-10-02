4b7ccf1d5154ee19fbb2e4d2d90b479d7653d7f2
FSW - Unit Conversion <egibson4@bucs.fsw.edu>
2024-10-02 16:24:46 -0400

    Added a couple equations for the conversions.

--- Java - Unit Conversion/src/Main.java
+++ Java - Unit Conversion/src/Main.java
@@ -2,7 +2,16 @@ public class Main
 {
     public static void main(String[] args)
     {
+        float inch = inch;
+        float foot = inch * 12;
+        float cubit = inch * 18;
+        float step = (float) (foot * 2.5);
+        float yard = foot * 3;
+        float mile = foot * 5280;
+
+        float inch = foot/12;
+
         // 12 inches = 1 foot, 18 inches = 1 cubit, 2.5 feet = 1 step, 3 feet = 1 yard, 5280 feet = 1 mile
-        System.out.print("Hello World!!");
+        System.out.print(inch);
     }
 }
